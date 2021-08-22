f0a8f8e53c4a948ac495646b3f1979bf0bb4b044
ting3242006 <ting3242006@hotmail.com>
2021-08-22 16:45:17 +0800

    Initial Commit

---
+++ shoppingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shoppingApp
+//
+//  Created by Ting on 2021/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
